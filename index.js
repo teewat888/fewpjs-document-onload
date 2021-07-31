23354f37736f2169d95d3c40a786a445b6c4393a
teerawat prarom <teewat@yahoo.com>
2021-08-01 01:14:07 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let p = document.getElementById("text");
+    p.innerHTML = "This is really cool!";
+  });
+  
+  
